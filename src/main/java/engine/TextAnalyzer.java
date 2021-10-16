ba337fd853270069cc7a9413cb4cbfeb8fd0b3c4
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2021-10-16 15:20:34 +0200

    Fix: closed stream to re-use it

--- src/main/java/engine/TextAnalyzer.java
+++ src/main/java/engine/TextAnalyzer.java
@@ -30,6 +30,7 @@ public class TextAnalyzer {
         while(tokenStream.incrementToken()) {
             result.add(attr.toString());
         }
+        tokenStream.close();
         return result;
     }
 
