52d7409dca6d370a12640182f3171c149ba68733
Kajman <kaim1396@gmail.com>
2021-11-06 22:37:36 +0100

    first launch properly javaFX
    -Added dependency (javaFX)
    -First "HELLO WORLD" in GUI

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module GraphLiterature {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.base;
+    requires gs.core;
+    requires java.logging;
+    requires lucene.core;
+    requires lucene.analyzers.stempel;
+    opens javaFX;
+}
