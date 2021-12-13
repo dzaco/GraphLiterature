0759475b35876be2443f1e252f4c5fbd2a81424c
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2021-12-13 12:03:08 +0100

    fix after merge

--- src/main/java/javaFX/Controller.java
+++ src/main/java/javaFX/Controller.java
@@ -6,6 +6,9 @@ import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.DirectoryChooser;
@@ -79,20 +82,6 @@ public class Controller{
         bookArea.setText(listString);
     }
 
-  @FXML
-    void handleBtnAnalizedWords(ActionEvent event) throws IOException {
-        System.out.println("WORKS");
-        Parent root = FXMLLoader.load(getClass().getResource("/statistic_view.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setTitle("Graph View");
-        stage.setScene(scene);
-        //modal new window
-        stage.initModality(Modality.WINDOW_MODAL); //default
-        stage.initOwner(btnNewWindowAnalizedWords.getScene().getWindow());
-        stage.show();
-    }
-
     @FXML
     public void handles(ActionEvent e) {
         Stage stage = (Stage) borderDirectory.getScene().getWindow(); // take stage from
@@ -110,35 +99,7 @@ public class Controller{
         }
         borderDirectory.setTop(a);
     }
-    public void handles() {
-        Stage stage = (Stage) borderDirectory.getScene().getWindow(); // take stage from
-        TreeView<String> a = new TreeView<String>();
-        DirectoryChooser dc = new DirectoryChooser();
-        dc.setInitialDirectory(new File(System.getProperty("user.home") + "\\Desktop\\GrapH_Project\\GraphLiterature\\src\\main\\resources\\Books")); //DekstopRequired
-        File choice = dc.showDialog(stage);
-        if(choice == null || ! choice.isDirectory()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText("Could not open directory");
-            alert.setContentText("The file is invalid.");
-            alert.showAndWait();
-        } else {
-            a.setRoot(getNodesForDirectory(choice));
-        }
-        borderDirectory.setTop(a);
-    }
-  @FXML
-    void handleBtnStatisticView(ActionEvent event) throws IOException {
-        System.out.println("WORKS");
-        Parent root = FXMLLoader.load(getClass().getResource("/analized_words_view.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setTitle("Graph View");
-        stage.setScene(scene);
-        //modal new window
-        stage.initModality(Modality.WINDOW_MODAL); //default
-        stage.initOwner(btnNewWindowStatisticView.getScene().getWindow());
-        stage.show();
-    }
+
     public TreeItem<String> getNodesForDirectory(File directory){
         TreeItem<String> root = new TreeItem<String>(directory.getName());
         for(File f : directory.listFiles()) {
