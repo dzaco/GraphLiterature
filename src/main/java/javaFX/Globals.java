3624e9e7132f5999c397f7baab604531173303b7
Kajman <kaim1396@gmail.com>
2022-01-11 17:22:39 +0100

    Create_Statistic_View

---
+++ src/main/java/javaFX/Globals.java
@@ -0,0 +1,9 @@
+package javaFX;
+
+import engine.TextStatistic;
+import engine.WordsStatistic;
+
+public class Globals {
+    public static TextStatistic textStat;
+    public static WordsStatistic wordStat;
+}
